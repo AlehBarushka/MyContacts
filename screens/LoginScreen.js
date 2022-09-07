c3196049011d83778d4440325f2afe7f54d70114
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-09-07 14:16:07 +0300

    Add React Native navigation (#2)

---
+++ screens/LoginScreen.js
@@ -0,0 +1,61 @@
+import {
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='Email' />
+        <TextInput style={styles.input} placeholder='Password' secureTextEntry />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    justifyContent: 'center',
+  },
+});
